47dca47832d635a042f4ab8daddb43b8c87d007a
Dan Fox <iamdanfox@gmail.com>
2014-06-03 17:16:53 +0100

    Post endpoint

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+
+app.post('/maillist-signup', function(req, res){
+  // send email to oumpa-subscribe-user=example.com@maillist.ox.ac.uk
+  res.send(req.query.email.replace("@","="))
+});
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
