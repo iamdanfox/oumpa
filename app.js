0779970cf0c3a1be8dcc87de1542b253b28d99e8
Dan Fox <iamdanfox@gmail.com>
2014-06-03 17:35:01 +0100

    Demo emails sent to my email address

--- app.js
+++ app.js
@@ -11,10 +11,39 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+var smtpTransport = mailer.createTransport();
 
 app.post('/maillist-signup', function(req, res){
   // send email to oumpa-subscribe-user=example.com@maillist.ox.ac.uk
-  res.send(req.query.email.replace("@","="))
+
+  var email = req.query.email
+  if (email) {
+
+    var dest = "oumpa-subscribe-"+email.replace("@","=")+"@maillist.ox.ac.uk"
+
+
+    var mail = {
+      from: "Node JS <nodejs@heroku.com>",
+      to: "iamdanfox@gmail.com",
+      //to: dest,
+      subject: "Send Email Using Node.js",
+      text: dest,
+      // html: "<b>Node.js New world for me</b>"
+    }
+
+    smtpTransport.sendMail(mail, function(error, response){
+        if(error){
+            console.log(error);
+            res.send(error, 500) // internal server error
+        }else{
+            console.log("Message sent: " + response.message);
+            res.send("Success")
+        }
+        // smtpTransport.close();
+    });
+  } else {
+    res.send("Error: please specify email parameter", 400) // bad request
+  }
 });
 
 var port = Number(process.env.PORT || 5000);
