785d67da17f2374eea9b5e8ac3fe249b9138e3a5
Dan Fox <iamdanfox@gmail.com>
2014-06-03 18:02:38 +0100

    Real email address

--- app.js
+++ app.js
@@ -4,45 +4,42 @@ var logfmt = require("logfmt");
 var mailer = require("nodemailer");
 
 var app = express();
+var smtpTransport = mailer.createTransport();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Sign up an email address to an Oxford Ezmlm list.\n\nSend a get '+
+  'request to /maillist-signup?email=first.last%40coll.ox.ac.uk\n\nUser will '+
+  'receive a confirmation email.');
 });
 
-var smtpTransport = mailer.createTransport();
-
-app.post('/maillist-signup', function(req, res){
+app.get('/maillist-signup', function(req, res){
   // send email to oumpa-subscribe-user=example.com@maillist.ox.ac.uk
 
-  var email = req.query.email
+  var email = req.query.email;
   if (email) {
-
-    var dest = "oumpa-subscribe-"+email.replace("@","=")+"@maillist.ox.ac.uk"
-
+    var dest = "oumpa-subscribe-"+email.replace("@","=")+"@maillist.ox.ac.uk";
 
     var mail = {
-      from: "Node JS <nodejs@heroku.com>",
-      to: "iamdanfox@gmail.com",
-      //to: dest,
-      subject: "Send Email Using Node.js",
-      text: dest,
-      // html: "<b>Node.js New world for me</b>"
-    }
+      from: "OUMPA <pentathlon.club@studentclubs.ox.ac.uk>", // email for OUMPA SSO
+      to: dest,
+      subject: "Online signup for OUMPA mailing list",
+      text: "Online signup for OUMPA mailing list"
+    };
 
     smtpTransport.sendMail(mail, function(error, response){
         if(error){
             console.log(error);
-            res.send(error, 500) // internal server error
+            res.send(error, 500); // internal server error
         }else{
             console.log("Message sent: " + response.message);
-            res.send("Success")
+            res.send("Success");
         }
         // smtpTransport.close();
     });
   } else {
-    res.send("Error: please specify email parameter", 400) // bad request
+    res.send("Error: please specify email parameter", 400); // bad request
   }
 });
 
