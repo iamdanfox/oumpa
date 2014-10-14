ab075cde17d5bace51c5239b9f92ab3e0f4aa11e
Dan Fox <iamdanfox@gmail.com>
2014-10-14 16:21:16 +0100

    More descriptive email

--- app.js
+++ app.js
@@ -21,8 +21,10 @@ app.get('/maillist-signup', function(req, res){
       from: email,
       to: "oumpa-subscribe@maillist.ox.ac.uk",
       cc: 'president@oxfordpentathlon.org, OUMPA <pentathlon.club@studentclubs.ox.ac.uk>',
-      subject: "Online signup for OUMPA mailing list",
-      text: email
+      subject: "[OUMPA mailing list notification] "+email+" just signed up for the OUMPA mailing list",
+      text: email + " just signed up for the OUMPA mailing list on oxfordpentathlon.org. \n\n" +
+        "You shouldn't need to do anything, but if this email looks like a tab, you can kick them off at " +
+        "http://web.maillist.ox.ac.uk/"
     };
 
     smtpTransport.sendMail(mail, function(error, response){
